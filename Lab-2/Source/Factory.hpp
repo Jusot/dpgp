a944e0f29aa2b7755ff5eedae79853c8aa369c25
mu001999 <mu001999@outlook.com>
2019-10-23 20:59:51 +0800

    update Factory

--- Lab-2/Source/Factory.hpp
+++ Lab-2/Source/Factory.hpp
@@ -1,10 +1,32 @@
 #pragma once
 
+#include <memory>
+
 namespace Lab2
 {
+class CPU;
+class Memory;
+class Mainboard;
+
 class Factory
 {
   public:
     virtual ~Factory() = default;
+    virtual std::unique_ptr<CPU>
+    createCPU() = 0;
+    virtual std::unique_ptr<Memory>
+    createMemroy() = 0;
+    virtual std::unique_ptr<Mainboard>
+    createMainboard() = 0;
+};
+
+class SamsungFactory : public Factory
+{
+
+};
+
+class IntelFactory : public Factory
+{
+
 };
 } // namespace Lab2
