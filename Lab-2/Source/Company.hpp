160d550e73240fa0874ecb1dd30be8b389ab98e2
mu001999 <mu001999@outlook.com>
2019-10-23 20:59:46 +0800

    update Company

--- Lab-2/Source/Company.hpp
+++ Lab-2/Source/Company.hpp
@@ -1,6 +1,27 @@
 #pragma once
 
+#include <memory>
+
 namespace Lab2
 {
+class PC;
+class Laptop;
 
+class Company
+{
+  public:
+    virtual ~Company() = default;
+    virtual std::unique_ptr<PC> orderPC(/* args */) = 0;
+    virtual std::unique_ptr<Laptop> orderLaptop(/* args */) = 0;
+};
+
+class CompanyA : public Company
+{
+
+};
+
+class CompanyB : public Company
+{
+
+};
 } // namespace Lab2
