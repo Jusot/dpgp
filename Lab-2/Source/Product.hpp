546417a7588cc9b166de79df8ee4d03ae9d5d3a7
mu001999 <mu001999@outlook.com>
2019-10-24 16:21:38 +0800

    update Product

--- Lab-2/Source/Product.hpp
+++ Lab-2/Source/Product.hpp
@@ -26,68 +26,12 @@ class Mainboard
     virtual void runTest() = 0;
 };
 
-class Computer
-{
-  public:
-    Computer(std::unique_ptr<CPU> cpu,
-        std::unique_ptr<Memory> memory,
-        std::unique_ptr<Mainboard> mainboard)
-      : cpu_(std::move(cpu)),
-        memory_(std::move(memory)),
-        mainboard_(std::move(mainboard_))
-    {
-        // ...
-    }
-
-    const CPU &getCPU()
-    {
-        return *cpu_;
-    }
-
-    const Memory &getMemory()
-    {
-        return *memory_;
-    }
-
-    const Mainboard &getMainboard()
-    {
-        return *mainboard_;
-    }
-
-  private:
-    std::unique_ptr<CPU> cpu_;
-    std::unique_ptr<Memory> memory_;
-    std::unique_ptr<Mainboard> mainboard_;
-};
-
-class PC : public Computer
-{
-  public:
-    PC(std::unique_ptr<CPU> cpu,
-        std::unique_ptr<Memory> memory,
-        std::unique_ptr<Mainboard> mainboard)
-      : Computer(std::move(cpu), std::move(memory),
-            std::move(mainboard))
-    {
-        // ...
-    }
-};
-
-class Laptop : public Computer
-{
-  public:
-    Laptop(std::unique_ptr<CPU> cpu,
-        std::unique_ptr<Memory> memory,
-        std::unique_ptr<Mainboard> mainboard)
-      : Computer(std::move(cpu), std::move(memory),
-            std::move(mainboard))
-    {
-        // ...
-    }
-};
-
 class SamsungCPU : public CPU
 {
-
+  public:
+    void runTest() override
+    {
+        // ...
+    }
 };
 } // namespace Lab2
